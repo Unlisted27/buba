a4b48f81b18ac09836634820cbdb6071e5c1b1c8
Unlisted_dev <archerhpo@gmail.com>
2025-05-22 14:28:13 -0300

    testing

--- apps/dev.bub/dev.py
+++ apps/dev.bub/dev.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 import socket, basics
-btn_select = basics.btn_select
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.settimeout(0)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.254.254.254', 1))
-        IP = s.getsockname()[0]
-    except Exception:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
-basics.scrnprint("My ip: "+get_ip())
-btn_select.wait_for_press()
+try:
+    btn_select = basics.btn_select
+    def get_ip():
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(0)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.254.254.254', 1))
+            IP = s.getsockname()[0]
+        except Exception:
+            IP = '127.0.0.1'
+        finally:
+            s.close()
+        return IP
+    basics.scrnprint("My ip: "+get_ip())
+    btn_select.wait_for_press()
+except Exception as e:
+    print(e)
+    basics.error_warn()
+    exit()
