e98b365d8d9fd67f61d386aa6caf63ee6422838f
Unlisted_dev <archerhpo@gmail.com>
2025-06-26 17:33:40 -0300

    ircloner test

--- apps/ircloner.bub/ircloner.py
+++ apps/ircloner.bub/ircloner.py
@@ -1,2 +1,26 @@
 #!/usr/bin/env python3
-print("IR cloner coming soon (:")
+import pigpio
+import time
+
+PIN = 17 #GPIO pin where the IR OUT is connected
+
+def cbf(gpio, level, tick):
+    print(f"GPIO: {gpio}, Level: {level}, Tick: {tick}")
+
+    
+pi = pigpio.pi()
+if not pi.connected:
+    exit()
+
+pi.set_mode(PIN, pigpio.INPUT)
+cb = pi.callback(PIN, pigpio.EITHER_EDGE, cbf)
+
+try:
+    print("Listening for raw IR signal...")
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    pass
+finally:
+    cb.cancel()
+    pi.stop()
