dd800fdf934554a169ce299df6e60561510c57b3
Unlisted_dev <archerhpo@gmail.com>
2025-06-27 00:35:37 -0300

    yeaa

--- apps/ircloner.bub/ircloner.py
+++ apps/ircloner.bub/ircloner.py
@@ -16,44 +16,6 @@ def test():
     pi.stop()
     #bubasics.btn_select.wait_for_press()
 
-def send_single_wave(data):
-    pi = pigpio.pi()
-    PIN     = 19
-    CARRIER = 38000
-
-    # Build a single pulse list for the entire sequence:
-    wf = []
-    for idx, duration in enumerate(data):
-        if duration < 10:
-            continue
-        if idx % 2 == 0:
-            # “mark”: generate carrier bursts for `duration`
-            cycles = int(CARRIER * duration / 1e6)
-            on  = int(1e6 / CARRIER / 2)
-            off = on
-            for _ in range(cycles):
-                wf.append(pigpio.pulse(1<<PIN, 0, on))
-                wf.append(pigpio.pulse(0, 1<<PIN, off))
-        else:
-            # “space”: LED off for `duration`
-            wf.append(pigpio.pulse(0, 0, duration))
-
-    # Clear, add, create, and send:
-    pi.wave_clear()
-    pi.wave_add_generic(wf)
-    wid = pi.wave_create()
-    if wid >= 0:
-        pi.wave_send_once(wid)
-        while pi.wave_tx_busy():
-            time.sleep(0.01)
-        pi.wave_delete(wid)
-    else:
-        print("Failed to create wave")
-
-    pi.stop()
-
-import pigpio, time
-
 def send_in_chunks(data, chunk_size=50):
     pi = pigpio.pi()
     PIN     = 19
@@ -96,63 +58,6 @@ def send_in_chunks(data, chunk_size=50):
             print("Chunk failed to create wave")
     pi.stop()
 
-def send(data):
-    #bubasics.clear_screen()
-    print("Sending data...")
-    #bubasics.scrnprint("Sending data...")
-    pi = pigpio.pi()
-    PIN = 19  # Must be a hardware PWM-capable GPIO (18 recommended)
-    CARRIER = 38000  # 38 kHz
-    marks_wid = {}
-    spaces_wid = {}
-    bad_waves = 0
-
-    wave = []
-
-    for i in range(len(data)):
-        duration = data[i]
-        if duration < 10:  # Skip junk durations
-            continue
-        if i % 2 == 0:
-            # Mark (LED on with modulation)
-            if duration not in spaces_wid:
-                pi.wave_add_generic([pigpio.pulse(0, 0, duration)])
-                wid = pi.wave_create()
-                if wid < 0:
-                    bad_waves += 1
-                    continue
-                spaces_wid[duration] = wid
-
-            # Only append if it now exists
-            if duration in spaces_wid:
-                wave.append(spaces_wid[duration])
-
-        else:
-            # Space (LED off)
-            #bubasics.error_warn() #DIDNT MAKE IT
-            if duration not in spaces_wid:
-                pi.wave_add_generic([pigpio.pulse(0, 0, duration)])
-                wid = pi.wave_create()
-                if wid < 0:
-                    bad_waves += 1
-                    continue
-                spaces_wid[duration] = wid
-        wave.append(spaces_wid[duration])
-    #bubasics.error_warn() DIDNT MAKE IT
-    pi.wave_chain(wave)
-    #bubasics.error_warn()
-    while pi.wave_tx_busy():
-        time.sleep(0.01)
-
-    for wid in marks_wid.values():
-        pi.wave_delete(wid)
-    for wid in spaces_wid.values():
-        pi.wave_delete(wid)
-    pi.wave_clear()
-    pi.stop()
-    print(f"Number of bad waves: {bad_waves}")
-    #bubasics.scrnprint(f"Number of bad waves: {bad_waves}")
-    #bubasics.btn_select.wait_for_press()
 
 def listen():
     os.system("sudo systemctl start pigpiod") #Need to start pigpiod (pigpio deamon) for this to work
