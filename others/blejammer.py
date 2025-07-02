c9668e296775894d40f03548e65115f6bd8ba4f1
Unlisted_dev <archerhpo@gmail.com>
2025-07-02 14:14:50 -0300

    aaa

--- others/blejammer.py
+++ others/blejammer.py
@@ -1,16 +1,14 @@
-import os
-import time
+import asyncio
+from bleak import BleakAdvertiser
 
-# Random BLE advertising with hciconfig and hcitool
-def advertise_loop():
-    while True:
-        os.system("sudo hciconfig hci0 noleadv")
-        os.system("sudo hcitool -i hci0 cmd 0x08 0x0008 1E 02 01 1A 1B FF 4C 00 02 15 " +
-                  "AA BB CC DD EE FF 00 00 00 00 00 00 00 00 C5 00 01")  # fake beacon data
-        os.system("sudo hciconfig hci0 leadv 3")
-        time.sleep(1)
+fake_devices = [f"FakeBLE_{i:03d}" for i in range(100)]
 
-try:
-    advertise_loop()
-except KeyboardInterrupt:
-    print("Stopped.")
+async def rotate_ads():
+    advertiser = BleakAdvertiser()
+    for device in fake_devices:
+        print(f"Advertising as {device}")
+        await advertiser.start(name=device)
+        await asyncio.sleep(0.2)
+        await advertiser.stop()
+
+asyncio.run(rotate_ads())
