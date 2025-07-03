4d321fffb4a52e28e872805e848f3ffee5c382d5
Unlisted_dev <archerhpo@gmail.com>
2025-07-02 21:21:29 -0300

    aa

---
+++ others/networkspoofer.py
@@ -0,0 +1,19 @@
+import subprocess
+
+ssid = "Free_Public_WiFi"
+hostapd_conf = f"""
+interface=wlan0
+driver=nl80211
+ssid={ssid}
+hw_mode=g
+channel=6
+auth_algs=1
+wmm_enabled=0
+ignore_broadcast_ssid=0
+"""
+
+with open("/tmp/fakeap.conf", "w") as f:
+    f.write(hostapd_conf)
+
+# Start hostapd
+subprocess.run(["sudo", "hostapd", "/tmp/fakeap.conf"])
