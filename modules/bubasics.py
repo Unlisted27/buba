4acd3042b374a5d229e8f2925d918f5516bab86f
Unlisted_dev <archerhpo@gmail.com>
2025-09-21 22:25:49 -0300

    aaaaa

--- modules/bubasics.py
+++ modules/bubasics.py
@@ -1,9 +1,9 @@
 import time,os,pathlib,json,subprocess, bubasicsconfig
-from gpiozero import Button
+from gpiozero import Button, Device
 #from gpiozero import Button
 from PIL import Image, ImageDraw, ImageFont
 
-GPIO.setwarnings(False)
+Device.pin_factory.set_warning(False)
 
 btn_up = Button(bubasicsconfig.buttons.btn_up_gpio, bounce_time = bubasicsconfig.buttons.bounce_time)
 btn_down = Button(bubasicsconfig.buttons.btn_down_gpio, bounce_time = bubasicsconfig.buttons.bounce_time)
