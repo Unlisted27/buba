e3778b685a3f0b43f9f55b3180533435156f0b80
Unlisted_dev <archerhpo@gmail.com>
2025-09-22 22:31:22 -0300

    asdasd

--- apps/ircloner.bub/ircloner.py
+++ apps/ircloner.bub/ircloner.py
@@ -1,5 +1,5 @@
 import serial, json
-ser = serial.Serial('/dev/serial0', 115200, timeout=1)
+ser = serial.Serial('/dev/serial0', 1200, timeout=1)
 ser.write(b'{"cmd":"start_scan"}\n')
 line = ser.readline().decode().strip()
 if line:
